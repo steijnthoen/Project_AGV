106bc7c768fb68887ef6ac4b2e5addb720e4c40e
Jurriaanmk <90131419+Jurriaanmk@users.noreply.github.com>
2022-06-10 12:12:15 +0200

    Update Hello world Test.c

--- Hello world Test.c
+++ Hello world Test.c
@@ -3,7 +3,7 @@ int main()
 {
    // idereen weet wel hoe een printf statement werkt neem ik aan
    printf("Hello, World! \n");
-   printf("Groetjes Jurriaan");
+   printf("Groetjes Jurriaan katsman");
    printf("Steijn is hier ook geweest");
 
    return 0;
