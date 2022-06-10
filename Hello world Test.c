86104efcecf5a0f933cb406bb0b68456068f4b6e
steijnthoen <38354274+steijnthoen@users.noreply.github.com>
2022-06-10 11:29:25 +0200

    Update Hello world Test.c

--- Hello world Test.c
+++ Hello world Test.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
-int main() {
-   // printf() displays the string inside quotation
+int main() 
+{
+   // idereen weet wel hoe een printf statement werkt neem ik aan
    printf("Hello, World! \n");
    printf("Groetjes Jurriaan");
+   printf("Steijn is hier ook geweest");
 
    return 0;
 }
