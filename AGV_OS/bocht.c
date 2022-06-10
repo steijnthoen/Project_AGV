61cc7297c22b6afcdad44924670bb27442f61a51
steijnthoen <38354274+steijnthoen@users.noreply.github.com>
2022-06-10 11:31:02 +0200

    Add files via upload

---
+++ AGV_OS/bocht.c
@@ -0,0 +1,67 @@
+
+int gyroscope_refrence;
+
+void mass_update_gyroscope()
+{
+	int i;
+	for(i = 0; i<15; i++)
+	{
+		gyroscope_update();
+	}
+}
+
+
+
+void start_gyroscope()
+{
+	gyroscope_init();
+	mass_update_gyroscope();
+	gyroscope_refrence = gyroscope_get_rotation_Z();
+}
+
+
+
+// uitgaande dat gyroscoop optelt wanneer draait met klok mee
+void bocht_rechts(int graden_bocht)
+{
+	mass_update_gyroscope();
+	gyroscope_refrence = gyroscope_get_rotation_Z();
+
+	int current_rotation = 0;
+
+	// start bocht rechts
+
+	while(current_rotation >= graden_bocht)
+	{
+		current_rotation = (gyroscope_get_rotation_Z() - gyroscope_refrence);
+		gyroscope_update();
+	}
+
+	// stop motoren
+}
+
+
+
+// uitgaande dat gyroscoop aftrekt wanneer draait tegen de klok in
+void bocht_links(int graden_bocht)
+{
+	mass_update_gyroscope();
+	gyroscope_refrence = gyroscope_get_rotation_Z();
+
+	int current_rotation = 0;
+
+	// start bocht links
+	while(current_rotation <= -graden_bocht)
+	{
+		current_rotation = (gyroscope_get_rotation_Z() - gyroscope_refrence);
+		if(current_rotation > 0)
+		{
+			current_rotation -= 360;
+		}
+
+		gyroscope_update();
+	}
+
+	// stop motoren
+}
+
